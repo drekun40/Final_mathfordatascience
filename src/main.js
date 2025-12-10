36fe0e49ab3427ac8d35bedb8194d67c1c09f920
Danylo Rekun <danylorekun@MacBook-Pro-Danylo.local>
2025-12-10 17:49:33 -0500

    chore: add version log to verify deployment

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import html from './htm.js';
 import App from './App.js';
 
 const root = createRoot(document.getElementById('root'));
+console.log("Mercyhurst App v2.0.3 - Button Fix Applied");
 root.render(html`<${App} />`);
